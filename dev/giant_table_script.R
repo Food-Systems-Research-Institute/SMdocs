306028446212286320423e8e3b5b7f4a03a309c1
Chris Donovan <118745720+ChrisDonovan307@users.noreply.github.com>
2025-09-10 18:56:40 -0400

    adding tiny trend graphs, giant latex table, adjusting coverage graph formatting and adding ggplot theme

--- dev/giant_table_script.R
+++ dev/giant_table_script.R
@@ -1,3 +1,24 @@
+# Giant Table Script
+# 2025-09-10
+
+#' This script takes our 'revised_secondary_metrics.xlsx' from OneDrive and
+#' combines it with the literature justifications to get one very large, some
+#' might even say giant, table. It may be saved back to OneDrive for reference,
+#' but it is not a final product. We will use this table to:
+#'   1. Create the body table that will go in the body of the paper. This 
+#'    will include just the essential information for the reader: dimension,
+#'    indicator, metric, definition, weighting, source, and shorthand citations.
+#'    Subject to revision.
+#'  2. Create the supplementary table with stuff for nerds: level of analysis,
+#'    indicator type, state coverage, county coverage, first year, last year, year
+#'    range, url, resolution, updates, probably others. I suppose it's just 
+#'    everything else that is of any use.
+
+#' TODO: Add units to body table.
+#' TODO: Make appendix table
+
+
+
 # Housekeeping ------------------------------------------------------------
 
 
@@ -11,108 +32,66 @@ pacman::p_load(
   readr,
   remotes,
   writexl,
-  httr2
+  httr2,
+  knitr,
+  kableExtra
 )
+pacman::p_load_gh('ChrisDonovan307/projecter')
 
 # Get fresh data
 remotes::install_github('Food-Systems-Research-Institute/SMdata', force = TRUE)
 library(SMdata)
 
 
+
 # Pull Metrics Revised and Lit Justification Excel files -----------------
+## Set Paths ---------------------------------------------------------------
 
 
-path <- "/Users/isabellaloconte/OneDrive - University of Vermont/Susannah Walsh Daloz's files - Sustainability Metrics Manuscript/Metrics/secondary_metrics_revised.xlsx"
+# If else statement to get paths to work based on who is running the script
+if (Sys.getenv('username') == 'isabellaloconte') {
+  root_metric_path <- "/Users/isabellaloconte/OneDrive - University of Vermont/Susannah Walsh Daloz's files - Sustainability Metrics Manuscript/Metrics/"
+  og_xl_path <- paste0(root_metric_path, "secondary_metrics_revised.xlsx")
+  lit_path <- paste0(root_metric_path, "literature_justifying_indicators.xlsx")
+} else if (Sys.getenv('username') == 'cdonov12') {
+  root_metric_path <- "C:/Users/cdonov12/OneDrive - University of Vermont/Food Systems Research Center/Sustainability Metrics/Sustainability Metrics Manuscript/Metrics/"
+  og_xl_path <- paste0(root_metric_path, "secondary_metrics_revised.xlsx")
+  lit_path <- paste0(root_metric_path, "literature_justifying_indicators.xlsx")
+}
 
-path2 <- "/Users/isabellaloconte/OneDrive - University of Vermont/Susannah Walsh Daloz's files - Sustainability Metrics Manuscript/Metrics/literature_justifying_indicators.xlsx"
-
-new_xl <- '2_clean/secondary_metrics.xlsx'
-
-
-# Read Excel files (5 sheets of Metrics file) ----------------------------
+# Relative path to where we will save a copy of the secondary metrics xl locally
+new_xl_path <- 'data/data_paper/secondary_metrics.xlsx'
 
 
-df1 <- read_excel(path)
-df2 <- read_excel(path2)
 
-sheet_names <- excel_sheets(path)
+## Read Revised Metrics files (5 sheets of Metrics file) -------------------
+
+
+og_xl <- read_excel(og_xl_path)
+sheet_names <- excel_sheets(og_xl_path)
 
 sheets_list <- lapply(sheet_names[1:5], function(sheet) {
   df <- read_excel(path, sheet = sheet)
   df$quality <- as.character(df$quality)
-  df
-})
-
-combined_df <- bind_rows(sheets_list)
-
-
-# Join Excel files --------------------------------------------------------
-
-
-metrics_and_justification <- combined_df %>%
-  left_join(df2 %>%
-              select(indicator,
-                     dimension = dimension,
-                     "Shorthand Citations" = "Shorthand Citations"),
-            by = "indicator") %>%
-  fill(index, indicator, `Shorthand Citations`, dimension, .direction = "down") %>%
-  select(quality, dimension, index, everything())
-
-
-View(metrics_and_justification)
-
-
-# Making mega table (filtering out things) --------------------------------
-
-
-mega_table <- metrics_and_justification %>%
-  select(-index, -`indicator type (Schreeful et al 2024)`, -quality, -keep, -analyze, -url, -notes, -status, -updates, -resolution, -desirable)
-
-View(mega_table)
-
-
-# Joining with current metric sheet BY CODE NOT SHEETS ---------------------
-
-
-# Create folder for cleaned data
-dir.create("2_clean", recursive = TRUE, showWarnings = FALSE)
-
-file.copy(path, new_xl, overwrite = TRUE)
-
-# Pull the working excel from OneDrive to yoink variable names and figure out
-# what we need to do
-sheets <- excel_sheets(new_xl)[1:5]
-tab <- map(sheets, ~ {
-  read_excel('2_clean/secondary_metrics.xlsx', sheet = .x) %>% 
-    mutate(
-      dimension = str_to_lower(.x), .before = 'index',
-      quality = as.character(quality)
-    ) %>% 
-    fill(c(index, indicator), .direction = 'down')
+  df$dimension <- sheet
+  return(df)
 }) %>% 
   bind_rows()
-get_str(tab)
+str(combined_df)
 
-# Also pull weighting variables from utilities sheet
-# add this to 5_objects so it gets lumped with sm_data and exported as rda
-util_sheet <- read_excel('2_clean/secondary_metrics.xlsx', sheet = 'utilities')
-utils <- util_sheet %>% 
-  filter(status != 'stall') %>% 
-  select(metric, variable_name)
 
-# Create a folder again
-dir.create("5_objects", showWarnings = FALSE)
 
-saveRDS(utils, '5_objects/weighting_vars.rds')
+## Summary Stats -----------------------------------------------------------
 
-# Pull them from our metrics
+
+# Pull out existing SMdata metrics
 existing_metrics <- metrics %>% 
-  filter(variable_name %in% tab$variable_name) %>% 
-  filter_fips('neast')
+  filter(variable_name %in% combined_df$variable_name) %>% 
+  SMdata::filter_fips('neast')
 get_str(existing_metrics)
 
-# Do existing first
-existing_metrics_sum <- existing_metrics %>% 
+# Get a summary table of existing metrics
+sum_stats <- existing_metrics %>% 
   group_by(variable_name) %>% 
   summarize(
     n_states = length(unique(fips[nchar(fips) == 2])),
@@ -122,36 +101,227 @@ existing_metrics_sum <- existing_metrics %>%
     latest_year = max(unique(year)),
     year_range = max(unique(as.numeric(year))) - min(unique(as.numeric(year)))
   )
-get_str(existing_metrics_sum)
+get_str(sum_stats)
 
-# Joining mega table with stats
-new_table_with_sum <- mega_table %>%
-  left_join(existing_metrics_sum %>%
-              select(variable_name,
-                     n_states = n_states,
-                     n_counties = n_counties,
-                     n_years = n_years,
-                     first_year = first_year,
-                     latest_year = latest_year,
-                     year_range = year_range),
-            by = "variable_name") %>%
-  select(-variable_name)
-                     
+# Join to combined_df
+new_table_with_sum <- combined_df %>%
+  left_join(sum_stats, by = 'variable_name')
 View(new_table_with_sum)
+get_str(new_table_with_sum)
+
+# Also join to SMdata metadata to get units for each variable
+# TODO: fix this and add units
+# out <- metadata %>% 
+#   select(variable_name, units) %>% 
+#   inner_join(new_table_with_sum, by = 'variable_name')
 
 
-# Save table with state, county, year stats to OneDrive -------------------
+## Join with Literature Justifications -------------------------------------
 
 
-new_path <- paste0(
-  "/Users/isabellaloconte/OneDrive - University of Vermont/Susannah Walsh Daloz's files - Sustainability Metrics Manuscript/Metrics/",
+lit_xl <- read_excel(lit_path)
+get_str(lit_xl)
+
+# Reformat shorthand citations to be separated by commas
+head(lit_xl$`Shorthand Citations`)
+lit_xl <- lit_xl %>% 
+  mutate(
+    shorthand_citations = `Shorthand Citations` %>% 
+      str_replace_all('\\r\\n', ', '),
+    .keep = 'unused'
+  )
+head(lit_xl$shorthand_citations)
+
+# Check indicator counts in lit_xl and combined_df
+lit_ind_count <- length(unique(lit_xl$indicator))
+df_ind_count <- length(unique(new_table_with_sum$indicator))
+if (lit_ind_count != df_ind_count) {
+  warning(
+    paste(
+      "There should be the same number of indicators in the lit xl and the summary table",
+      'The lit XL has', lit_ind_count, 'and the summary table has', df_ind_count
+    )
+  )
+}
+
+# Combine summary table with justification
+giant_table <- new_table_with_sum %>%
+  left_join(
+    select(lit_xl, indicator, shorthand_citations),
+    by = "indicator"
+  ) %>%
+  fill(indicator, index, shorthand_citations, .direction = "down") %>%
+  select(quality, dimension, everything())
+View(giant_table)
+get_str(giant_table)
+
+
+
+# Making mega table (filtering out things) --------------------------------
+
+
+# # Remove columns using any_of() with a vector so that it will still work
+# # even if the column does not exist after any changes
+# cols <- c(
+#   'index',
+#   'indicator type (Schreeful et al 2024)',
+#   'quality',
+#   'keep',
+#   'analyze',
+#   'url',
+#   'notes',
+#   'status',
+#   'updates',
+#   'resolution',
+#   'desirable'
+# )
+# mega_table <- metrics_and_justification %>%
+#   select(-any_of(cols))
+# 
+# View(mega_table)
+# str(mega_table)
+
+
+
+# Utils Data Object -------------------------------------------------------
+
+
+# While we're fiddling with excel sheets, let's pull out the weighting variable
+# names that we will use in the SMdocs project.
+util_sheet <- read_excel(
+  og_xl_path,
+  sheet = 'utilities'
+)
+utils <- util_sheet %>% 
+  filter(status != 'stall') %>% 
+  select(metric, variable_name)
+
+# Save to data folder
+saveRDS(utils, 'data/data_paper/weighting_vars.rds')
+
+
+
+# Save Giant Table to OneDrive --------------------------------------------
+
+
+# Note that this is not a final product, just a way to reference our giant table
+giant_table_path <- paste0(
+  root_metric_path,
   Sys.Date(),
-  '_table_for_paper.xlsx'
+  '_giant_table.xlsx'
 )
 
 openxlsx2::write_xlsx(
-  new_table_with_sum, 
-  new_path,
+  giant_table, 
+  giant_table_path,
   widths = c(15, 'auto'),
   na.strings = 'NA'
 )
+
+
+
+# Body Table --------------------------------------------------------------
+
+
+# Pulling from giant table, here we pull out relevant columns for the table that
+# goes into the body of the paper and make a latex table. We are also putting in a placeholder column for our trend graphs.
+
+# TODO: Work out cell merging in latex table for dimension, citations
+trend_files <- dir('outputs/trend_plots/')
+body_table <- giant_table %>% 
+  select(
+    dimension,
+    indicator,
+    metric,
+    definition,
+    weighting,
+    source,
+    variable_name # use this to link to trend graph, then drop
+    # shorthand_citations
+  ) %>% 
+  # Where we have a metric, include an image of trend
+  mutate(
+    trend = case_when(
+      (metric != 'NONE' & !is.na(variable_name) & 
+         str_detect(paste(trend_files, collapse = "|"), variable_name)) ~ paste0(
+        '\\includegraphics[width=\\hsize,valign=c]{figures/trend_figures/fig_trend_', 
+        variable_name,
+        '.png}'
+      ),
+      .default = NA_character_
+    ),
+    .after = definition,
+  ) %>% 
+  mutate(
+    # Manually escape (so that we don't excape in kbl())
+    across(
+      everything(),
+      ~ .x %>%
+        str_replace_all('%', ' percent') %>%
+        str_replace_all('\\$', ' dollars')
+    ),
+    # Put a space between Tradition and Heritage
+    indicator = case_when(
+      str_detect(indicator, 'Tradition') ~ 'Tradition and Heritage',
+      .default = indicator
+    )
+  ) %>% 
+  select(-variable_name) %>% 
+  # Format headers
+  setNames(c(names(.) %>% snakecase::to_title_case()))
+  
+get_str(body_table)
+body_table$Trend
+  
+# Get baseline table
+body_latex <- body_table %>% 
+  kbl(
+    format = 'latex',
+    caption = 'Metric Attributes and Trends',
+    label = 'tab_metrics',
+    escape = FALSE
+  ) %>% 
+  kable_styling(
+    font_size = 10
+  )
+body_latex
+
+# Remove existing table formatting
+body <- body_latex %>% 
+  str_split_i("\\\\begin\\{tabular\\}\\[t\\]\\{[^}]+\\}\\n", 2) %>%
+  str_replace("^\\\\begin\\{tabular\\}\\[t\\]\\{[^}]+\\}\\s*", "") %>% 
+  str_remove_all('\\\\hline\n') %>% 
+  str_remove('\\\\end\\{tabular\\}\n\\\\end\\{table\\}')
+body %>% 
+  cat()
+
+# Add our own header and footer
+header <- '\\begin{landscape}
+\\scriptsize
+\\begin{longtblr}[caption = Metric Attributes]{
+  % colspec={Q[50] Q[200] Q[200] Q[200]}, % Column widths,
+  rowhead = 1,
+  row{1} = {font=\\bfseries},
+  colsep = 2pt, % Spaces between column lines and text/figure
+  cells={halign=c,valign=m}, % Center horizontal and vertical
+  column{1-2}={wd=2cm},
+  column{3}={wd=3cm},
+  column{4}={wd=4cm},
+  column{5-6}={wd=2cm},
+  column{7-Z}={wd=3cm},
+  vlines,
+  % hline{1,2,Z}={solid} % for only header and footer
+  hlines, % all horizontal lines
+}
+\\label{tab:tab_metrics_body}
+'
+
+footer <- '\\end{longtblr}
+\\end{landscape}'
+
+# Put them all together
+body_out <- paste0(header, body, footer)
+cat(body_out)
+
+# Save this to latex file
+writeLines(body_out, 'outputs/tab_body_metrics.tex')
